6ca4e41526a9257a1cd32ca9736caa7eea824532
Atika Rizki <37141007+atikakiki@users.noreply.github.com>
2018-10-08 23:30:05 +0700

    Delete tambah.php

--- tambah.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nama   = $_POST['nama'];
-$no_telepon   = $_POST['no_telepon'];
-$alamat   = $_POST['alamat'];
-
-include "knk.php";
-mysql_query ("insert into addressbook(nama, no_telepon, alamat) values('$nama','$no_telepon','$alamat')")
-or die ("menambah gagal.");
-echo "Menambah data berhasil";  
